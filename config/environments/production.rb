3f67251154dc209b39e788d8db3492df579db0dd
Samantha Silva <samanthasilva@grupofortes.com.br>
2019-07-02 16:17:38 -0300

    Ajustando.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,8 @@ Rails.application.configure do
     :domain => 'smtp.mailtrap.io',
     :port => '2525',
     :authentication => :cram_md5
-
+    
+  }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
